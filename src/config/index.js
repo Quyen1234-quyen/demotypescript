02d08a9fdc83b899c5e810187b5a52c3d7ba7269
Quyen <85078797+Quyen1234-quyen@users.noreply.github.com>
2021-06-22 09:53:49 +0700

    Delete index.js

--- src/config/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports["default"] = {
-    port: 1511,
-    host: 'localhost',
-    dbUri: 'mongodb+srv://quyen:1@cluster0.yutwk.mongodb.net/quyen?retryWrites=true&w=majority'
-};
